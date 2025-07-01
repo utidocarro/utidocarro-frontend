a4a38bf3258adc18f4e6ae9af46b759205d12cd2
ramon541 <ramondiasmonteiro@gmail.com>
2025-07-01 20:45:23 -0300

    UTI-78 :sparkles: feat: Adiciona as configurações do github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import * as path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@interfaces": path.resolve(__dirname, "./src/interfaces"),
-      "@services": path.resolve(__dirname, "./src/services"),
-      "@store": path.resolve(__dirname, "./src/store"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@libs": path.resolve(__dirname, "./src/libs"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@routes": path.resolve(__dirname, "./src/routes"),
-      "@": path.resolve(__dirname, "./src"),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@interfaces': path.resolve(__dirname, './src/interfaces'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@libs': path.resolve(__dirname, './src/libs'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   plugins: [react()],
+  base: '/utidocarro-frontend',
 });
